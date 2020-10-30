1052b0b72689f483273e61280ebb8261c222b06b
Bhavesh0510 <bhaveshshelar0510@gmail.com>
2020-10-30 17:42:41 +0530

    "added some php"

---
+++ HTML/register.php
@@ -0,0 +1,17 @@
+<?php
+  $fname = $_POST['fname'];
+  echo $fname;
+  $lname = $_POST['lname'];
+  echo $lname;
+  $e = $_POST['email'];
+  echo $e;
+  $age = $_POST['age'];
+  echo $age;
+  $pass = $_POST['password'];
+  echo $pass;
+  $cpass = $_POST['confirmpassword'];
+  echo $cpass;
+  $g = $_POST['optradio'];
+  echo $g;
+  
+?>
